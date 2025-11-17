a4244f85434fafa545b433b369acb9fa7c6b0764
lilnapolesbase9-maker <lilnapolesbase9@gmail.com>
2025-11-17 18:45:30 +0000

    domin finish

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronombre = ["los", "la", "el"];
+  let adjetivo = ["mejores", "bomba", "crema"];
+  let nombre = ["bocadillos", "derelojeria", "canela"];
+  let dominio = [".com", ".es", ".onion"];
+
+
+
+  for (let p = 0; p < pronombre.length; p++) {
+    for (let a = 0; a < adjetivo.length; a++) {
+      for (let n = 0; n < nombre.length; n++) {
+        for (let d = 0; d < dominio.length; d++) {
+
+          console.log(pronombre[p]+ adjetivo[a]+ nombre[n]+ dominio[d]
+          );
+
+        }
+      }
+    }
+  }
 };
